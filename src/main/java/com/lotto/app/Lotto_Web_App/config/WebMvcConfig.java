f52fc2df665354b5e8c51c4deb28d8ddb2340e24
abderafi3 <109274205+abderafi3@users.noreply.github.com>
2024-05-26 02:17:35 +0200

    Update WebMvcConfig.java

--- src/main/java/com/lotto/app/Lotto_Web_App/config/WebMvcConfig.java
+++ src/main/java/com/lotto/app/Lotto_Web_App/config/WebMvcConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableScheduling
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
