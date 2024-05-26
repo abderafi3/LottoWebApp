acfabc36bc81e077df1e3a8d241c90ea8992629c
abderafi3 <109274205+abderafi3@users.noreply.github.com>
2024-05-26 10:18:39 +0200

    Update LottoWebAppApplication.java

--- src/main/java/com/lotto/app/Lotto_Web_App/LottoWebAppApplication.java
+++ src/main/java/com/lotto/app/Lotto_Web_App/LottoWebAppApplication.java
@@ -4,9 +4,10 @@ import com.lotto.app.Lotto_Web_App.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LottoWebAppApplication {
 	@Autowired
 	private EmailService emailService;
