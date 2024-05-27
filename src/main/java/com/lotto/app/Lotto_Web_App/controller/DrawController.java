23b746e269e1654a3765f4440aa292e480705554
abderafi3 <abderafia@gmail.com>
2024-05-27 09:22:23 +0200

    code refactoring

--- src/main/java/com/lotto/app/Lotto_Web_App/controller/DrawController.java
+++ src/main/java/com/lotto/app/Lotto_Web_App/controller/DrawController.java
@@ -49,25 +49,4 @@ public class DrawController {
         return response;
     }
 
-//    @GetMapping("/ticketMatchCounts")
-//    public Map<Integer, Long> getTicketMatchCounts() {
-//        Draw lastDraw = drawService.findLastDraw();
-//        Map<Integer, Long> matchCounts = new HashMap<>();
-//        for (int i = 3; i <= 6; i++) {
-//            matchCounts.put(i, 0L);
-//        }
-//
-//        if (lastDraw != null) {
-//            List<Integer> winningNumbers = lastDraw.getWinningNumbers();
-//            List<Ticket> tickets = ticketService.findAllTickets();
-//
-//            for (Ticket ticket : tickets) {
-//                long matchCount = ticket.getNumberSet().stream().filter(winningNumbers::contains).count();
-//                if (matchCount >= 3 && matchCount <= 6) {
-//                    matchCounts.put((int) matchCount, matchCounts.getOrDefault((int) matchCount, 0L) + 1);
-//                }
-//            }
-//        }
-//        return matchCounts;
-//    }
 }
